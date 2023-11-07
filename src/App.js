3325b8253b0b36361ad3c900c62cf8d27284b77b
Rasif Sahl <rasifsahl@Rasifs-MacBook-Pro.local>
2023-11-07 23:52:17 +0530

    code enhancments

--- src/App.js
+++ src/App.js
@@ -5,15 +5,14 @@ function App() {
     <div className="App">
       <header className="App-header">
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Theming with react
         </p>
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
-          rel="noopener noreferrer"
         >
-          Learn React
+          Learn React...
         </a>
       </header>
     </div>
